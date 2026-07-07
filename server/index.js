7bff9262f3bbdb21e254cfea757df1c6c1c0fc90
Vigneshkumartcse <vigneshkumar.t2022csec@sece.ac.in>
2026-07-07 22:57:49 +0530

    ftyu

--- server/index.js
+++ server/index.js
@@ -22,8 +22,6 @@ if (!fs.existsSync(uploadsDir)) {
 
 initializeApp();
 
-app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
-app.use(compression());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
@@ -31,6 +29,8 @@ app.use((req, res, next) => {
   if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
+app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
+app.use(compression());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
