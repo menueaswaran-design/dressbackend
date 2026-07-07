d63c10d9ce7c51d951b0ea064d1e559d8007d9bb
Vigneshkumartcse <vigneshkumar.t2022csec@sece.ac.in>
2026-07-07 22:43:46 +0530

    f

--- server/index.js
+++ server/index.js
@@ -24,7 +24,13 @@ initializeApp();
 
 app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(compression());
-app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+  next();
+});
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
